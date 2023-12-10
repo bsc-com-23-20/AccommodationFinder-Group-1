12fa258b60b46877ce508e7411925a3034926148
Geoffrey Makawa <bed-com-36-18@unima.ac.mw>
2023-12-10 08:12:34 +0200

    Setting up redux store complete. Redux toolkit testing done

--- src/App.js
+++ src/App.js
@@ -1,82 +1,80 @@
 import Login from './pages/login/login';
 import Register from './pages/register/register';
-import * as ReactDOM from "react-dom/client";
+import * as ReactDOM from 'react-dom/client';
 import {
   createBrowserRouter,
   RouterProvider,
   Outlet,
-  Navigate
-} from "react-router-dom";
-import Navbar from "./components/navbar/navbar";
-import Rightbar from "./components/rightbar/rightbar";
-import Leftbar from "./components/leftbar/leftbar";
-import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
+  Navigate,
+} from 'react-router-dom';
+import Navbar from './components/navbar/navbar';
+import Rightbar from './components/rightbar/rightbar';
+import Leftbar from './components/leftbar/leftbar';
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
 //import AuthContext from "./components/context/authContext";
-import useContext from "react";
+import useContext from 'react';
+import Counter from './components/counter/Counter';
 
 function App() {
-
   const currentUser = true;
 
-  const Layout = ()=>{
-
-    return(
-        <div>
-          <Navbar />
-          {/* <div style ={{display: "Flex"}}>
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <Counter />
+        {/* <div style ={{display: "Flex"}}>
           <Leftbar/> 
           <Outlet/>
           <Rightbar/> 
           </div> */}
-        </div>
-    )
-  }
+      </div>
+    );
+  };
 
-  const ProtectedRoute = ({children}) => {
-    
-    if (!currentUser){
-      return <Navigate to="/login"/>;
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to='/login' />;
     }
 
-    return children
-  }
+    return children;
+  };
 
   const router = createBrowserRouter([
-
     {
-      path: "/",
+      path: '/',
       element: (
-      <ProtectedRoute>
-        <Layout/>
-      </ProtectedRoute>
-        ),
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
       children: [
         {
-        path: "/home",
-        element: <Home/>,
-      },
+          path: '/home',
+          element: <Home />,
+        },
 
-      {
-        path: "/profile/:id",
-        element: <Profile/>,
-      }
-    ]
+        {
+          path: '/profile/:id',
+          element: <Profile />,
+        },
+      ],
     },
 
     {
-      path: "/login",
-      element: <Login/>,
+      path: '/login',
+      element: <Login />,
     },
 
     {
-      path: "/register",
-      element: <Register/>,
+      path: '/register',
+      element: <Register />,
     },
-  ])
+  ]);
 
   return (
-    <div className="App">
+    <div className='App'>
       <RouterProvider router={router} />
     </div>
   );
