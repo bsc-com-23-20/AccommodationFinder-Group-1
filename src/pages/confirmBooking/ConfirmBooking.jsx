f9f86dff813fbad6c02587ae2dfb3516c24ab84c
Geoffrey Makawa <bed-com-36-18@unima.ac.mw>
2023-12-11 08:04:13 +0200

    Confirm Booking Component Just Need Dynamic Card Rendering, Implemented Static Rendering Only

--- src/pages/confirmBooking/ConfirmBooking.jsx
+++ src/pages/confirmBooking/ConfirmBooking.jsx
@@ -1,11 +1,118 @@
 import ArrowBack from '@mui/icons-material/ArrowBack';
-import React from 'react';
+import React, { useState } from 'react';
 import photo_geoff from './images/geoff.jpg';
 import Transactio_ID from './images/Transaction ID.jpg';
 
 import './ConfirmBooking.css';
+import { nanoid } from '@reduxjs/toolkit';
 
 const ConfirmBooking = () => {
+  //   const [prospectiveTenant, setProspectiveTenant] = useState([
+  //     {
+  //       id: nanoid(),
+  //       name: 'user',
+  //       phoneNumber: '0992787820',
+  //       email: 'johnbanda@gmail.com',
+  //       photo: photo_geoff,
+  //       transaction_id: Transactio_ID,
+  //     },
+  //     {
+  //       id: nanoid(),
+  //       name: 'user',
+  //       phoneNumber: '0992787820',
+  //       email: 'johnbanda@gmail.com',
+  //       photo: photo_geoff,
+  //       transaction_id: Transactio_ID,
+  //     },
+  //     {
+  //       id: nanoid(),
+  //       name: 'user',
+  //       phoneNumber: '0992787820',
+  //       email: 'johnbanda@gmail.com',
+  //       photo: photo_geoff,
+  //       transaction_id: Transactio_ID,
+  //     },
+  //     {
+  //       id: nanoid(),
+  //       name: 'user',
+  //       phoneNumber: '0992787820',
+  //       email: 'johnbanda@gmail.com',
+  //       photo: photo_geoff,
+  //       transaction_id: Transactio_ID,
+  //     },
+  //     {
+  //       id: nanoid(),
+  //       name: 'user',
+  //       phoneNumber: '0992787820',
+  //       email: 'johnbanda@gmail.com',
+  //       photo: photo_geoff,
+  //       transaction_id: Transactio_ID,
+  //     },
+  //     {
+  //       id: nanoid(),
+  //       name: 'user',
+  //       phoneNumber: '0992787820',
+  //       email: 'johnbanda@gmail.com',
+  //       photo: photo_geoff,
+  //       transaction_id: Transactio_ID,
+  //     },
+  //     {
+  //       id: nanoid(),
+  //       name: 'user',
+  //       phoneNumber: '0992787820',
+  //       email: 'johnbanda@gmail.com',
+  //       photo: photo_geoff,
+  //       transaction_id: Transactio_ID,
+  //     },
+  //     {
+  //       id: nanoid(),
+  //       name: 'user',
+  //       phoneNumber: '0992787820',
+  //       email: 'johnbanda@gmail.com',
+  //       photo: photo_geoff,
+  //       transaction_id: Transactio_ID,
+  //     },
+  //     {
+  //       id: nanoid(),
+  //       name: 'user',
+  //       phoneNumber: '0992787820',
+  //       email: 'johnbanda@gmail.com',
+  //       photo: photo_geoff,
+  //       transaction_id: Transactio_ID,
+  //     },
+  //     {
+  //       id: nanoid(),
+  //       name: 'user',
+  //       phoneNumber: '0992787820',
+  //       email: 'johnbanda@gmail.com',
+  //       photo: photo_geoff,
+  //       transaction_id: Transactio_ID,
+  //     },
+  //     {
+  //       id: nanoid(),
+  //       name: 'user',
+  //       phoneNumber: '0992787820',
+  //       email: 'johnbanda@gmail.com',
+  //       photo: photo_geoff,
+  //       transaction_id: Transactio_ID,
+  //     },
+  //     {
+  //       id: nanoid(),
+  //       name: 'user',
+  //       phoneNumber: '0992787820',
+  //       email: 'johnbanda@gmail.com',
+  //       photo: photo_geoff,
+  //       transaction_id: Transactio_ID,
+  //     },
+  //     {
+  //       id: nanoid(),
+  //       name: 'user',
+  //       phoneNumber: '0992787820',
+  //       email: 'johnbanda@gmail.com',
+  //       photo: photo_geoff,
+  //       transaction_id: Transactio_ID,
+  //     },
+  //   ]);
   return (
     <section className='confirm-booking-container'>
       <header>
@@ -19,16 +126,20 @@ const ConfirmBooking = () => {
       <main>
         <div className='top'>
           <div className='profile-pic-container'>
-            <img className='profile-pic' src={photo_geoff} alt='' />
+            <img
+              className='profile-pic'
+              src={photo_geoff}
+              alt='student_photo'
+            />
           </div>
           <div className='name-container'>
-            <p>student name</p>
-            <p>Phone Number</p>
+            <p>Student Name</p>
             <p>Email</p>
+            <p>Phone Number</p>
           </div>
           <div className='trans-id-container'>
             <div className='trans-id-here'>
-              <img src={Transactio_ID} alt='trans_ID' />
+                <img src={Transactio_ID} alt="" />
             </div>
           </div>
           <div className='btn-container'>
